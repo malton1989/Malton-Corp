e469d9b6e0ef6efc2ba2e26998af0b694005a124
Anton <tosha.malyamov@yandex.ru>
2017-12-28 13:42:33 +0300

    1st version
    Unfinished

---
+++ corporate.loc/resources/views/pink/contact_bar.blade.php
@@ -0,0 +1,26 @@
+
+				            <div class="widget-first widget contact-info">
+				                <h3>Contacts</h3>
+				                <div class="sidebar-nav">
+				                    <ul>
+				                        <li>
+				                            <i class="icon-map-marker" style="color:#979797;font-size:20pxpx"></i> Location: Saint-Peterburg
+				                        </li>
+				                        <li>
+				                            <i class="icon-info-sign" style="color:#979797;font-size:20pxpx"></i> Phone: +7 963 124 54 64 
+				                        </li>
+				                        <li>
+				                            <i class="icon-print" style="color:#979797;font-size:20pxpx"></i> Skype: tosha.malyamov
+				                        </li>
+				                        <li>
+				                            <i class="icon-envelope" style="color:#979797;font-size:20pxpx"></i> Email: tosha.malyamov@yandex.ru
+				                        </li>
+				                    </ul>
+				                </div>
+				            </div>
+				            <div class="widget-last widget text-image">
+				                <h3>Customer Support</h3>
+				                <div class="text-image" style="text-align:left"><img src="images/callus.gif" alt="Customer Support" /></div>
+				                <p>Nunc sit amet pretium purus. Pellet netus et malesuada fames ac turpis egestas.entesque habitant morbi tristique senectus </p>
+				            </div>
+				        
